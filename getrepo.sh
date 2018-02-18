e8f8fb54636b01e5330e44215be11dd0dc78b999
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-02-18 16:51:41 -0300

    Update getrepo.sh

--- getrepo.sh
+++ getrepo.sh
@@ -1,2 +1,3 @@
 #svn export https://github.com/username/repo-name/trunk/
+svn export https://github.com/andersonalmeidax0/util/trunk/
 svn export https://github.com/andersonalmeidax0/util/trunk/src/lib/text.txt

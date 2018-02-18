7d945c19eb85fed4137c113ab3b45bc8ee5767b8
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-02-18 16:48:23 -0300

    Create installdocker.sh

---
+++ installdocker.sh
@@ -0,0 +1,19 @@
+curl -fsSL get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+#install docker-ce on debian 9 (stretch)
+#sudo apt-get update
+#sudo apt-get install \
+#     apt-transport-https \
+##     ca-certificates \
+#     curl \
+#     gnupg2 \
+#     software-properties-common
+#gpg... skp
+#deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+#   $(lsb_release -cs) \
+#   stable > docker.rep.txt
+#sudo apt-get update
+#sudo apt-get install docker-ce
+#sudo docker run hello-world
+
+   
